fe97c7bdc3df1d2a3ca73d815f1d0449f6cbbb5a
AC <aloycwl@gmail.com>
2025-04-08 04:52:50 +0000

    Auto

--- freetier/models.py
+++ freetier/models.py
@@ -6,7 +6,8 @@ def md(cm, co) -> str:
   ro = [{"role":"system","content":cm},{"role":"user","content":co}]
   se = sy.argv[1]
 
-  if se == "gemini":
+  if True:
+  # if se == "gemini":
     from google import genai as ga
     from google.genai import types
     from modelGemini import mo
@@ -16,21 +17,21 @@ def md(cm, co) -> str:
       config=types.GenerateContentConfig(temperature=0.1)
     ).text)
 
-  elif se == "groq":
+  # elif se == "groq":
     from modelGroq import mo
     from groq import Groq as gr
     print(gr(api_key=os.getenv("QK")).chat.completions.create(model=mo(),messages=ro).choices[0].message.content)
 
-  elif se == "ollama":
-    import ollama as ol
-    from modelOllama import mo
-    print(ol.chat(model=mo(),messages=ro)["message"]["content"])
+  # elif se == "ollama":
+  #   import ollama as ol
+  #   from modelOllama import mo
+  #   print(ol.chat(model=mo(),messages=ro)["message"]["content"])
 
-  else:
-    import requests as rq
-    from modelCloudflare import mo
-    print(rq.post(
-      f"https://api.cloudflare.com/client/v4/accounts/{os.getenv('CA')}/ai/run/{mo()}",
-      headers={"Authorization": f"Bearer {os.getenv('CK')}",
-      "Content-Type": "application/json",
-      },json={"messages": ro}).json())
+  # else:
+  #   import requests as rq
+  #   from modelCloudflare import mo
+  #   print(rq.post(
+  #     f"https://api.cloudflare.com/client/v4/accounts/{os.getenv('CA')}/ai/run/{mo()}",
+  #     headers={"Authorization": f"Bearer {os.getenv('CK')}",
+  #     "Content-Type": "application/json",
+  #     },json={"messages": ro}).json())
